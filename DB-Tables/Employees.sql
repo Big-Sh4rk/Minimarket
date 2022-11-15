fe98e03a45147976a6748a43e91b6f3ab1742c12
BigShark <bigsh4ark@gmail.com>
2022-11-15 20:37:00 -0300

    Querys de Employees & Register terminadas

---
+++ DB-Tables/Employees.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees (
+id int NOT NULL PRIMARY KEY,
+name varchar(50) NOT NULL,
+last_name varchar(50) NOT NULL,
+age int NOT NULL,
+date_employed date NOT NULL,
+tel bigint NOT NULL,
+dni bigint NOT NULL,
+cuil bigint NOT NULL
+)
---
+++ DB-Tables/Register.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Register (
+id bigint NOT NULL PRIMARY KEY,
+employee_id int NOT NULL REFERENCES employees (id),
+hour_entry time with time zone NOT NULL,
+hour_out time with time zone NOT NULL,
+day_date date
+)
